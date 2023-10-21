4cafe64223a239080dac1003d0fc6ae8134c4d87
fuinss <80682227+fuinss@users.noreply.github.com>
2023-10-21 17:37:22 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -43,11 +43,8 @@ config_xray() {
 	read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
 	START_PORT=${START_PORT:-$DEFAULT_START_PORT}
 	if [ "$config_type" == "socks" ]; then
-		read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
-		SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
-
-		read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
-		SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
+		SOCKS_USERNAME=""
+		SOCKS_PASSWORD=""
 	elif [ "$config_type" == "vmess" ]; then
 		read -p "UUID (默认随机): " UUID
 		UUID=${UUID:-$DEFAULT_UUID}
@@ -55,36 +52,8 @@ config_xray() {
 		WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
 	fi
 
-	for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
-		config_content+="[[inbounds]]\n"
-		config_content+="port = $((START_PORT + i))\n"
-		config_content+="protocol = \"$config_type\"\n"
-		config_content+="tag = \"tag_$((i + 1))\"\n"
-		config_content+="[inbounds.settings]\n"
-		if [ "$config_type" == "socks" ]; then
-			config_content+="auth = \"password\"\n"
-			config_content+="udp = true\n"
-			config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
-			config_content+="[[inbounds.settings.accounts]]\n"
-			config_content+="user = \"$SOCKS_USERNAME\"\n"
-			config_content+="pass = \"$SOCKS_PASSWORD\"\n"
-		elif [ "$config_type" == "vmess" ]; then
-			config_content+="[[inbounds.settings.clients]]\n"
-			config_content+="id = \"$UUID\"\n"
-			config_content+="[inbounds.streamSettings]\n"
-			config_content+="network = \"ws\"\n"
-			config_content+="[inbounds.streamSettings.wsSettings]\n"
-			config_content+="path = \"$WS_PATH\"\n\n"
-		fi
-		config_content+="[[outbounds]]\n"
-		config_content+="sendThrough = \"${IP_ADDRESSES[i]}\"\n"
-		config_content+="protocol = \"freedom\"\n"
-		config_content+="tag = \"tag_$((i + 1))\"\n\n"
-		config_content+="[[routing.rules]]\n"
-		config_content+="type = \"field\"\n"
-		config_content+="inboundTag = \"tag_$((i + 1))\"\n"
-		config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
-	done
+	# ... other configuration code
+
 	echo -e "$config_content" >/etc/xrayL/config.toml
 	systemctl restart xrayL.service
 	systemctl --no-pager status xrayL.service
@@ -93,13 +62,15 @@ config_xray() {
 	echo "起始端口:$START_PORT"
 	echo "结束端口:$(($START_PORT + $i - 1))"
 	if [ "$config_type" == "socks" ]; then
-		echo "socks账号:$SOCKS_USERNAME"
-		echo "socks密码:$SOCKS_PASSWORD"
+		echo "socks账号: (无用户名)"
+		echo "socks密码: (无密码)"
 	elif [ "$config_type" == "vmess" ]; then
 		echo "UUID:$UUID"
 		echo "ws路径:$WS_PATH"
 	fi
 	echo ""
+}
+
 }
 main() {
 	[ -x "$(command -v xrayL)" ] || install_xray
