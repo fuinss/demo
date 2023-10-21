eb674ab56109729d60617515fd63560ab9bf4556
fuinss <80682227+fuinss@users.noreply.github.com>
2023-10-21 17:36:11 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -1,4 +1,6 @@
 DEFAULT_START_PORT=20000                         #默认起始端口
+DEFAULT_SOCKS_USERNAME="userb"                   #默认socks账号
+DEFAULT_SOCKS_PASSWORD="passwordb"               #默认socks密码
 DEFAULT_WS_PATH="/ws"                            #默认ws路径
 DEFAULT_UUID=$(cat /proc/sys/kernel/random/uuid) #默认随机UUID
 
@@ -30,7 +32,6 @@ EOF
 	systemctl start xrayL.service
 	echo "Xray 安装完成."
 }
-
 config_xray() {
 	config_type=$1
 	mkdir -p /etc/xrayL
@@ -41,7 +42,13 @@ config_xray() {
 
 	read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
 	START_PORT=${START_PORT:-$DEFAULT_START_PORT}
-	if [ "$config_type" == "vmess" ]; then
+	if [ "$config_type" == "socks" ]; then
+		read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
+		SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
+
+		read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
+		SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
+	elif [ "$config_type" == "vmess" ]; then
 		read -p "UUID (默认随机): " UUID
 		UUID=${UUID:-$DEFAULT_UUID}
 		read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
@@ -58,6 +65,9 @@ config_xray() {
 			config_content+="auth = \"password\"\n"
 			config_content+="udp = true\n"
 			config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
+			config_content+="[[inbounds.settings.accounts]]\n"
+			config_content+="user = \"$SOCKS_USERNAME\"\n"
+			config_content+="pass = \"$SOCKS_PASSWORD\"\n"
 		elif [ "$config_type" == "vmess" ]; then
 			config_content+="[[inbounds.settings.clients]]\n"
 			config_content+="id = \"$UUID\"\n"
@@ -82,13 +92,15 @@ config_xray() {
 	echo "生成 $config_type 配置完成"
 	echo "起始端口:$START_PORT"
 	echo "结束端口:$(($START_PORT + $i - 1))"
-	if [ "$config_type" == "vmess" ]; then
+	if [ "$config_type" == "socks" ]; then
+		echo "socks账号:$SOCKS_USERNAME"
+		echo "socks密码:$SOCKS_PASSWORD"
+	elif [ "$config_type" == "vmess" ]; then
 		echo "UUID:$UUID"
 		echo "ws路径:$WS_PATH"
 	fi
 	echo ""
 }
-
 main() {
 	[ -x "$(command -v xrayL)" ] || install_xray
 	if [ $# -eq 1 ]; then
@@ -105,5 +117,4 @@ main() {
 		config_xray "socks"
 	fi
 }
-
 main "$@"
