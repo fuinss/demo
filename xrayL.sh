ac37851bc7d6c9d1f82e7d475254e08178ea3370
fuinss <80682227+fuinss@users.noreply.github.com>
2023-10-21 18:47:51 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -1,43 +1,8 @@
-#!/bin/bash
-
-DEFAULT_START_PORT=20000                          # Default starting port
-DEFAULT_WS_PATH="/ws"                             # Default WebSocket path
-DEFAULT_UUID=$(cat /proc/sys/kernel/random/uuid)  # Default random UUID
-
-IP_ADDRESSES=($(hostname -I))
-
-install_xray() {
-    echo "Installing Xray..."
-    apt-get install unzip -y || yum install unzip -y
-    wget https://github.com/XTLS/Xray-core/releases/download/v1.8.3/Xray-linux-64.zip
-    unzip Xray-linux-64.zip
-    mv xray /usr/local/bin/xrayL
-    chmod +x /usr/local/bin/xrayL
-    cat <<EOF >/etc/systemd/system/xrayL.service
-[Unit]
-Description=XrayL Service
-After=network.target
-
-[Service]
-ExecStart=/usr/local/bin/xrayL -c /etc/xrayL/config.json
-Restart=on-failure
-User=nobody
-RestartSec=3
-
-[Install]
-WantedBy=multi-user.target
-EOF
-    systemctl daemon-reload
-    systemctl enable xrayL.service
-    systemctl start xrayL.service
-    echo "Xray installation completed."
-}
-
-config_xray() {
-    config_type="http"
+config_http_proxy() {
     mkdir -p /etc/xrayL
+    config_type="http"
 
-    read -p "Starting port (default $DEFAULT_START_PORT): " START_PORT
+    read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
     START_PORT=${START_PORT:-$DEFAULT_START_PORT}
 
     for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
@@ -48,6 +13,8 @@ config_xray() {
         config_content+="[inbounds.settings]\n"
         config_content+="udp = true\n"
         config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
+        config_content+="[inbounds.streamSettings]\n"
+        config_content+="network = \"tcp\"\n\n"
         config_content+="[[outbounds]]\n"
         config_content+="sendThrough = \"${IP_ADDRESSES[i]}\"\n"
         config_content+="protocol = \"freedom\"\n"
@@ -57,19 +24,12 @@ config_xray() {
         config_content+="inboundTag = \"tag_$((i + 1))\"\n"
         config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
     done
-    echo -e "$config_content" >/etc/xrayL/config.json
+    echo -e "$config_content" >/etc/xrayL/config.toml
     systemctl restart xrayL.service
     systemctl --no-pager status xrayL.service
     echo ""
-    echo "HTTP proxy configuration completed"
-    echo "Starting port: $START_PORT"
-    echo "Ending port: $((START_PORT + $i - 1))"
+    echo "生成 HTTP 代理配置完成"
+    echo "起始端口:$START_PORT"
+    echo "结束端口:$(($START_PORT + $i - 1))"
     echo ""
 }
-
-main() {
-    [ -x "$(command -v xrayL)" ] || install_xray
-    config_xray
-}
-
-main
