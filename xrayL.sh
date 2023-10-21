915e7d1043466424bde922b95d344fb4ac0ab04f
fuinss <80682227+fuinss@users.noreply.github.com>
2023-10-21 18:58:50 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
-
-DEFAULT_START_PORT=20000                         # 默认起始端口
-DEFAULT_WS_PATH="/ws"                            # 默认ws路径
-DEFAULT_UUID=$(cat /proc/sys/kernel/random/uuid) # 默认随机UUID
+DEFAULT_START_PORT=20000                         #默认起始端口
+DEFAULT_SOCKS_USERNAME="userb"                   #默认socks账号
+DEFAULT_SOCKS_PASSWORD="passwordb"               #默认socks密码
+DEFAULT_WS_PATH="/ws"                            #默认ws路径
+DEFAULT_UUID=$(cat /proc/sys/kernel/random/uuid) #默认随机UUID
 
 IP_ADDRESSES=($(hostname -I))
 
 install_xray() {
-    echo "安装 Xray..."
-    apt-get install unzip -y || yum install unzip -y
-    wget https://github.com/XTLS/Xray-core/releases/download/v1.8.3/Xray-linux-64.zip
-    unzip Xray-linux-64.zip
-    mv xray /usr/local/bin/xrayL
-    chmod +x /usr/local/bin/xrayL
-    cat <<EOF >/etc/systemd/system/xrayL.service
+	echo "安装 Xray..."
+	apt-get install unzip -y || yum install unzip -y
+	wget https://github.com/XTLS/Xray-core/releases/download/v1.8.3/Xray-linux-64.zip
+	unzip Xray-linux-64.zip
+	mv xray /usr/local/bin/xrayL
+	chmod +x /usr/local/bin/xrayL
+	cat <<EOF >/etc/systemd/system/xrayL.service
 [Unit]
 Description=XrayL Service
 After=network.target
 
 [Service]
-ExecStart=/usr/local/bin/xrayL -c /etc/xrayL/config.json
+ExecStart=/usr/local/bin/xrayL -c /etc/xrayL/config.toml
 Restart=on-failure
 User=nobody
 RestartSec=3
@@ -27,22 +27,26 @@ RestartSec=3
 [Install]
 WantedBy=multi-user.target
 EOF
-    systemctl daemon-reload
-    systemctl enable xrayL.service
-    systemctl start xrayL.service
-    echo "Xray 安装完成."
+	systemctl daemon-reload
+	systemctl enable xrayL.service
+	systemctl start xrayL.service
+	echo "Xray 安装完成."
 }
+# ... [Previous part of the script remains unchanged]
 
 config_xray() {
     config_type=$1
     mkdir -p /etc/xrayL
-    if [ "$config_type" != "http" ] && [ "$config_type" != "vmess" ]; then
-        echo "类型错误！仅支持http和vmess."
+    if [ "$config_type" != "socks" ] && [ "$config_type" != "vmess" ]; then
+        echo "类型错误！仅支持socks和vmess."
         exit 1
     fi
 
     read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
     START_PORT=${START_PORT:-$DEFAULT_START_PORT}
+
+    # The username and password are no longer required for SOCKS configuration.
+    # The script now assumes no authentication (username/password is blank).
 
     if [ "$config_type" == "vmess" ]; then
         read -p "UUID (默认随机): " UUID
@@ -52,65 +56,34 @@ config_xray() {
     fi
 
     for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
-        config_content+="{
-  \"inbounds\": [
-    {
-      \"port\": $((START_PORT + i)),
-      \"protocol\": \"$config_type\",
-      \"settings\": {
-        \"udp\": true
-      },
-      \"tag\": \"tag_$((i + 1))\"
-    }
-  ],
-  \"outbounds\": [
-    {
-      \"protocol\": \"freedom\",
-      \"settings\": {},
-      \"tag\": \"tag_$((i + 1))\"
-    }
-  ],
-  \"routing\": {
-    \"rules\": [
-      {
-        \"type\": \"field\",
-        \"inboundTag\": [
-          \"tag_$((i + 1))\"
-        ],
-        \"outboundTag\": \"tag_$((i + 1))\"
-      }
-    ]
-  }
-}
-"
-        if [ "$config_type" == "vmess" ]; then
-            config_content+=",{
-  \"inbounds\": [
-    {
-      \"protocol\": \"vmess\",
-      \"port\": $((START_PORT + i)),
-      \"tag\": \"tag_$((i + 1))\",
-      \"settings\": {
-        \"clients\": [
-          {
-            \"id\": \"$UUID\"
-          }
-        ]
-      },
-      \"streamSettings\": {
-        \"network\": \"ws\",
-        \"wsSettings\": {
-          \"path\": \"$WS_PATH\"
-        }
-      }
-    }
-  ]
-}"
+        config_content+="[[inbounds]]\n"
+        config_content+="port = $((START_PORT + i))\n"
+        config_content+="protocol = \"$config_type\"\n"
+        config_content+="tag = \"tag_$((i + 1))\"\n"
+        config_content+="[inbounds.settings]\n"
+        if [ "$config_type" == "socks" ]; then
+            # For a SOCKS server with no authentication, we avoid setting accounts.
+            config_content+="auth = \"noauth\"\n"  # No authentication
+            config_content+="udp = true\n"
+            config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
+        elif [ "$config_type" == "vmess" ]; then
+            config_content+="[[inbounds.settings.clients]]\n"
+            config_content+="id = \"$UUID\"\n"
+            config_content+="[inbounds.streamSettings]\n"
+            config_content+="network = \"ws\"\n"
+            config_content+="[inbounds.streamSettings.wsSettings]\n"
+            config_content+="path = \"$WS_PATH\"\n\n"
         fi
-
-        config_content+=$'\n'
+        config_content+="[[outbounds]]\n"
+        config_content+="sendThrough = \"${IP_ADDRESSES[i]}\"\n"
+        config_content+="protocol = \"freedom\"\n"
+        config_content+="tag = \"tag_$((i + 1))\"\n\n"
+        config_content+="[[routing.rules]]\n"
+        config_content+="type = \"field\"\n"
+        config_content+="inboundTag = \"tag_$((i + 1))\"\n"
+        config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
     done
-    echo "$config_content" >/etc/xrayL/config.json
+    echo -e "$config_content" >/etc/xrayL/config.toml
     systemctl restart xrayL.service
     systemctl --no-pager status xrayL.service
     echo ""
@@ -124,20 +97,22 @@ config_xray() {
     echo ""
 }
 
+# ... [The rest of the script remains unchanged]
+
 main() {
-    [ -x "$(command -v xrayL)" ] || install_xray
-    if [ $# -eq 1 ]; then
-        config_type="$1"
-    else
-        read -p "选择生成的节点类型 (http/vmess): " config_type
-    fi
-    if [ "$config_type" == "vmess" ]; then
-        config_xray "vmess"
-    elif [ "$config_type" == "http" ]; then
-        config_xray "http"
-    else
-        echo "未正确选择类型，使用默认http配置."
-        config_xray "http"
-    fi
+	[ -x "$(command -v xrayL)" ] || install_xray
+	if [ $# -eq 1 ]; then
+		config_type="$1"
+	else
+		read -p "选择生成的节点类型 (socks/vmess): " config_type
+	fi
+	if [ "$config_type" == "vmess" ]; then
+		config_xray "vmess"
+	elif [ "$config_type" == "socks" ]; then
+		config_xray "socks"
+	else
+		echo "未正确选择类型，使用默认sokcs配置."
+		config_xray "socks"
+	fi
 }
 main "$@"
