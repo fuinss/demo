df172a4f35eb877e3445d6f2d3795bae6b960fa4
fuinss <80682227+fuinss@users.noreply.github.com>
2023-10-21 17:47:25 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -32,44 +32,73 @@ EOF
 	systemctl start xrayL.service
 	echo "Xray 安装完成."
 }
+# ... [Previous part of the script remains unchanged]
+
 config_xray() {
-	config_type=$1
-	mkdir -p /etc/xrayL
-	if [ "$config_type" != "socks" ] && [ "$config_type" != "vmess" ]; then
-		echo "类型错误！仅支持socks和vmess."
-		exit 1
-	fi
+    config_type=$1
+    mkdir -p /etc/xrayL
+    if [ "$config_type" != "socks" ] && [ "$config_type" != "vmess" ]; then
+        echo "类型错误！仅支持socks和vmess."
+        exit 1
+    fi
 
-	read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
-	START_PORT=${START_PORT:-$DEFAULT_START_PORT}
-	if [ "$config_type" == "socks" ]; then
-		SOCKS_USERNAME=""
-		SOCKS_PASSWORD=""
-	elif [ "$config_type" == "vmess" ]; then
-		read -p "UUID (默认随机): " UUID
-		UUID=${UUID:-$DEFAULT_UUID}
-		read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
-		WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
-	fi
+    read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
+    START_PORT=${START_PORT:-$DEFAULT_START_PORT}
 
-	# ... other configuration code
+    # The username and password are no longer required for SOCKS configuration.
+    # The script now assumes no authentication (username/password is blank).
 
-	echo -e "$config_content" >/etc/xrayL/config.toml
-	systemctl restart xrayL.service
-	systemctl --no-pager status xrayL.service
-	echo ""
-	echo "生成 $config_type 配置完成"
-	echo "起始端口:$START_PORT"
-	echo "结束端口:$(($START_PORT + $i - 1))"
-	if [ "$config_type" == "socks" ]; then
-		echo "socks账号: (无用户名)"
-		echo "socks密码: (无密码)"
-	elif [ "$config_type" == "vmess" ]; then
-		echo "UUID:$UUID"
-		echo "ws路径:$WS_PATH"
-	fi
-	echo ""
+    if [ "$config_type" == "vmess" ]; then
+        read -p "UUID (默认随机): " UUID
+        UUID=${UUID:-$DEFAULT_UUID}
+        read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
+        WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
+    fi
+
+    for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
+        config_content+="[[inbounds]]\n"
+        config_content+="port = $((START_PORT + i))\n"
+        config_content+="protocol = \"$config_type\"\n"
+        config_content+="tag = \"tag_$((i + 1))\"\n"
+        config_content+="[inbounds.settings]\n"
+        if [ "$config_type" == "socks" ]; then
+            # For a SOCKS server with no authentication, we avoid setting accounts.
+            config_content+="auth = \"noauth\"\n"  # No authentication
+            config_content+="udp = true\n"
+            config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
+        elif [ "$config_type" == "vmess" ]; then
+            config_content+="[[inbounds.settings.clients]]\n"
+            config_content+="id = \"$UUID\"\n"
+            config_content+="[inbounds.streamSettings]\n"
+            config_content+="network = \"ws\"\n"
+            config_content+="[inbounds.streamSettings.wsSettings]\n"
+            config_content+="path = \"$WS_PATH\"\n\n"
+        fi
+        config_content+="[[outbounds]]\n"
+        config_content+="sendThrough = \"${IP_ADDRESSES[i]}\"\n"
+        config_content+="protocol = \"freedom\"\n"
+        config_content+="tag = \"tag_$((i + 1))\"\n\n"
+        config_content+="[[routing.rules]]\n"
+        config_content+="type = \"field\"\n"
+        config_content+="inboundTag = \"tag_$((i + 1))\"\n"
+        config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
+    done
+    echo -e "$config_content" >/etc/xrayL/config.toml
+    systemctl restart xrayL.service
+    systemctl --no-pager status xrayL.service
+    echo ""
+    echo "生成 $config_type 配置完成"
+    echo "起始端口:$START_PORT"
+    echo "结束端口:$(($START_PORT + $i - 1))"
+    if [ "$config_type" == "vmess" ]; then
+        echo "UUID:$UUID"
+        echo "ws路径:$WS_PATH"
+    fi
+    echo ""
 }
+
+# ... [The rest of the script remains unchanged]
+
 main() {
 	[ -x "$(command -v xrayL)" ] || install_xray
 	if [ $# -eq 1 ]; then
